321fc56c191dec2f2cc0229d89efbaa511b4a58f
essanedev <essanedev@users.noreply.github.com>
2025-12-12 22:04:21 +0300

    Update production.py

--- Palette/settings/production.py
+++ Palette/settings/production.py
@@ -42,6 +42,3 @@ EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-
-STATIC_ROOT = '/staticfiles'
-MEDIA_ROOT = '/media'
