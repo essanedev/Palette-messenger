86bb855120336d112f1b680fa58ba959f43e49d9
essanedev <essanedev@users.noreply.github.com>
2025-12-12 20:47:12 +0300

    Update entrypoint.prod.sh

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -6,14 +6,7 @@ mkdir -p ./logs ./static ./staticfiles ./media
 chmod -R 0777 ./logs ./static ./staticfiles ./media || true
 chown -R palette-user:palette-user ./logs ./static ./staticfiles ./media 2>/dev/null || true
 
-#python manage.py collectstatic --noinput
-#python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+python manage.py migrate --noinput
 
-#exec python -m daphne -b 0.0.0.0 -p 8000 Palette.asgi:application
-
-# run collect/migrate as palette-user (su -s syntax runs the command as that user)
-su -s /bin/sh -c "python manage.py collectstatic --noinput" palette-user
-su -s /bin/sh -c "python manage.py migrate --noinput" palette-user
-
-# run daphne as palette-user
-exec su -s /bin/sh -c "python -m daphne -b 0.0.0.0 -p 8000 Palette.asgi:application" palette-user
+exec python -m daphne -b 0.0.0.0 -p 8000 Palette.asgi:application
