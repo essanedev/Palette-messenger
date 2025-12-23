6ad4e3c68fb34d6a031238e88d40193bfbdd5583
essanedev <essanedev@users.noreply.github.com>
2025-12-23 15:07:40 +0300

    Update entrypoint.prod.sh

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -9,6 +9,7 @@ chmod -R 0777 ./logs || true
 chown -R palette-user:palette-user ./logs 2>/dev/null || true
 
 python manage.py collectstatic --noinput
+python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
 exec python -m daphne -b 0.0.0.0 -p 8000 Palette.asgi:application
